f006b1206dc39c99bd50092dcdde3e5d6f299f28
Timm Friebe <thekid@thekid.de>
2013-08-22 21:06:23 +0200

    - Initial parser

--- src/main/php/org/yaml/YamlParser.class.php
+++ src/main/php/org/yaml/YamlParser.class.php
@@ -2,4 +2,12 @@
 
 class YamlParser extends \lang\Object {
 
+  public function parse(\io\streams\TextReader $reader) {
+    $r= array();
+    while (null !== ($line= $reader->readLine())) {
+      sscanf($line, "%[^:]: %[^\r]", $key, $value);
+      $r[$key]= $value;
+    }
+    return $r;
+  }
 }
--- src/test/php/org/yaml/YamlParserTest.class.php
+++ src/test/php/org/yaml/YamlParserTest.class.php
@@ -6,4 +6,26 @@ class YamlParserTest extends \unittest\TestCase {
   public function can_create() {
     new YamlParser();
   }
+
+  protected function parse($str) {
+    return create(new YamlParser())->parse(new \io\streams\TextReader(new \io\streams\MemoryInputStream($str)));
+  }
+
+  #[@test]
+  public function parse_empty() {
+    $this->assertEquals(array(), $this->parse(''));
+  }
+
+  #[@test]
+  public function parse_single_key_value() {
+    $this->assertEquals(array('key' => 'value'), $this->parse('key: value'));
+  }
+
+  #[@test]
+  public function parse_key_value() {
+    $this->assertEquals(
+      array('time' => '20:03:20', 'player' => 'Sammy Sosa', 'action' => 'strike (miss)'),
+      $this->parse("time: 20:03:20\nplayer: Sammy Sosa\naction: strike (miss)")
+    );
+  }
 }
