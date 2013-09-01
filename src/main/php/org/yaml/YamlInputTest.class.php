cf51c1f65800dc6d3c53271d4113ddec9f26610f
Timm Friebe <thekid@thekid.de>
2013-09-01 12:53:33 +0200

    - Initial release

---
+++ src/main/php/org/yaml/YamlInputTest.class.php
@@ -0,0 +1,63 @@
+<?php namespace org\yaml;
+
+use io\streams\TextReader;
+use io\streams\MemoryInputStream;
+
+class YamlInputTest extends AbstractYamlParserTest {
+
+  /**
+   * Creates a new fixture
+   *
+   * @param  string str
+   * @return org.yaml.Input
+   */
+  protected function newFixture($str= '') {
+    return new ReaderInput(new TextReader(new MemoryInputStream($str)));
+  }
+
+  #[@test]
+  public function nextLine_for_empty_input() {
+    $this->assertNull($this->newFixture('')->nextLine());
+  }
+
+  #[@test]
+  public function nextLine_for_non_empty_input() {
+    $this->assertEquals('Hello', $this->newFixture('Hello')->nextLine());
+  }
+
+  #[@test, @values(array("\r", "\n", "\r\n"))]
+  public function nextLine_for_one_line_input($delimiter) {
+    $this->assertEquals('Hello', $this->newFixture('Hello'.$delimiter)->nextLine());
+  }
+
+  #[@test, @values(array("\r", "\n", "\r\n"))]
+  public function nextLine_for_two_lines_of_input($delimiter) {
+    $fixture= $this->newFixture('Line 1'.$delimiter.'Line 2'.$delimiter);
+    $this->assertEquals(
+      array('Line 1', 'Line 2'),
+      array($fixture->nextLine(), $fixture->nextLine())
+    );
+  }
+
+  #[@test]
+  public function resetLine() {
+    $fixture= $this->newFixture('Hello');
+    $fixture->resetLine($fixture->nextLine());
+    $this->assertEquals('Hello', $fixture->nextLine());
+  }
+
+  #[@test]
+  public function matching() {
+    $this->assertEquals('hello', $this->newFixture()->matching('[hello]', '[', ']'));
+  }
+
+  #[@test]
+  public function matching_nested() {
+    $this->assertEquals('[hello]', $this->newFixture()->matching('[[hello]]', '[', ']'));
+  }
+
+  #[@test]
+  public function matching_nested2() {
+    $this->assertEquals('[hello],[world]', $this->newFixture()->matching('[[hello],[world]]', '[', ']'));
+  }
+}
